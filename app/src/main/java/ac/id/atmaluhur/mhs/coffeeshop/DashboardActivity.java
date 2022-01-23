0808fb4d072a07ecc12ac9b125114096365a7a46
IrgiRiz <irgitempilang28@gmail.com>
2022-01-23 22:34:39 +0700

    upload

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/coffeeshop/DashboardActivity.java
@@ -0,0 +1,25 @@
+package ac.id.atmaluhur.mhs.coffeeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+    }
+
+
+}
